f3bbe9af94e5634050700a3d9cbaf29412810938
gnmmarechal <gnmpolicemata91@gmail.com>
2016-06-13 16:26:31 +0100

    First Commit

---
+++ PrecisionSaves/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PrecisionSaves
+{
+    partial class mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu_strip = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // menu_strip
+            // 
+            this.menu_strip.Location = new System.Drawing.Point(0, 0);
+            this.menu_strip.Name = "menu_strip";
+            this.menu_strip.Size = new System.Drawing.Size(867, 24);
+            this.menu_strip.TabIndex = 0;
+            this.menu_strip.Text = "menuStrip1";
+            // 
+            // mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 571);
+            this.Controls.Add(this.menu_strip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menu_strip;
+            this.Name = "mainform";
+            this.Text = "Not-Datel PrecisionSaves";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu_strip;
+    }
+}
+
